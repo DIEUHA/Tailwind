f5d7174f0624167bacc1173e114b2595d8106e21
DIEUHA <sunidieu@gmail.com>
2025-01-08 17:21:12 +0700

    update code 4th

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        headerColor: "var(--header-color)", 
+        bgColor: "var(--background-color)",
+        breadcrumbColor: "var(--breadcrumb-background-color)",
+        textColor: "var(--text-color)",
+        borderColor: "var(--border-color)"
+        // Sử dụng biến CSS
+      },
+    },
+  },
+  plugins: [
+    function ({ addBase }) {
+      addBase({
+        ":root": {
+          "--header-color": "white", 
+          "--background-color": "#f9faff",
+          "--breadcrumb-background-color": "#f2f6f9",
+          "--text-color": "#494949",
+          "--border-color": "#dbdbdb",
+          // Định nghĩa biến CSS ở đây
+        },
+      });
+    },
+  ],
+};
